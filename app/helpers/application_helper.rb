ca78397edc4e2ba6b2edf290ac0840558e61ebef
YokoyamaGen <g.yokoyama.0616@gmail.com>
2021-02-27 07:23:45 +0900

    #13 レビューでの指摘事項を反映 max_widthにdevise_controllerの判定処理を移動

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,8 @@
 module ApplicationHelper
   def max_width
-    'mw-xl'
+    devise_controller? ? 'mw-md' : 'mw-xl'
   end
 
-  def devise_max_width
-    'mw-md'
-  end
-  
   def not_logged_in
     "disabled" unless user_signed_in?
   end
