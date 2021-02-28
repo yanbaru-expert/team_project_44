a1d4644c6ad7df22fd09b7740cf8cfb92e0b1acd
miya <rinn.sasami@gmail.com>
2021-02-28 15:27:04 +0900

    texts_controller.rb のindex メソッドに@textsを設定

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -2,5 +2,7 @@ class TextsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    # ()にマッチするレコードを全て取得する
+    @texts = Text.where("genre IN ('Basic', 'Git', 'HTML&CSS', 'Ruby', 'Ruby on Rails')")
   end
 end
