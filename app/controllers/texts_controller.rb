8e9efe9a3b7833ebda831fccb1f7c658cc2bba8b
miya <rinn.sasami@gmail.com>
2021-03-04 16:29:04 +0900

    texts_controllerとroutesの設定

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -5,4 +5,8 @@ class TextsController < ApplicationController
     # ()にマッチするレコードを全て取得する
     @texts = Text.where(genre: ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"])
   end
+
+  def show
+    @text = Text.find(params[:id])
+  end
 end
