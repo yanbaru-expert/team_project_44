ab5ce72e31b02b2aaa74c25781253213798893fe
miya <rinn.sasami@gmail.com>
2021-04-02 13:39:23 +0900

    text.data.csvとtextインポート機能の修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,15 +10,6 @@ User.find_or_create_by!(email: EMAIL) do |user|
   puts 'ユーザーの初期データインポートに成功しました。'
 end
 
-Text.delete_all
-CSV.foreach('db/csv_data/text_data.csv', headers: true) do |row|
-  Text.create(
-    genre: row['genre'],
-    title: row['title'],
-    content: row['content']
-  )
-end
-
 Movie.delete_all
 CSV.foreach('db/csv_data/movie_data.csv', headers: true) do |row|
   Movie.create(
@@ -32,6 +23,9 @@ AdminUser.find_or_create_by!(email: ADMIN_EMAIL) do |admin_user|
   admin_user.password_confirmation = ADMIN_PASSWORD
   puts "管理者ユーザーの初期データのインポートに成功しました。"
 end
+
+Text.delete_all
+ImportCsv.text_import('db/csv_data/text_data.csv')
 
 Question.destroy_all
 ImportCsv.question_import('db/csv_data/question_data.csv')
--- lib/autoloads/import_csv.rb
+++ lib/autoloads/import_csv.rb
@@ -1,5 +1,16 @@
 class ImportCsv
   # CSVデータのパスを引数として受け取り、インポート処理を実行
+  def self.text_import(path)
+    CSV.foreach(path, headers: true) do |row|
+      Text.create!(
+        genre: row['genre'],
+        title: row['title'],
+        content: row['content']
+      )
+    end
+    puts 'Text教材へのCSVデータ投入に成功しました。'
+  end
+
   def self.question_import(path)
     CSV.foreach(path, headers: true) do |row|
       Question.create!(
