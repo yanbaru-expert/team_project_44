6fa33eb1aa45a4b45240d63096dab1684219dcd5
yukimi.y <yyy.yukimi@gmail.com>
2021-04-03 16:38:22 +0900

    クリックで表示変更されるようview修正

---
+++ app/controllers/reads_controller.rb
@@ -0,0 +1,11 @@
+class ReadsController < ApplicationController
+  def create
+    current_user.reads.create!(text_id: params[:text_id])
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    current_user.reads.find_by(text_id: params[:text_id]).destroy!
+    redirect_back(fallback_location: root_path)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root "texts#index"
-  resources :texts, only: [:index, :show]
+  resources :texts, only: [:index, :show] do
+    resource :reads, only: [:create, :destroy]
+  end
   devise_for :users
   resources :movies
 end
