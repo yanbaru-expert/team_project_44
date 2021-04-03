fc8868f7b26b719e2beae9989a6e2f65b8a963b5
yukimi.y <yyy.yukimi@gmail.com>
2021-04-03 14:45:28 +0900

    Readモデルとテーブル作成し，多対多の関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :reads, dependent: :destroy
+  has_many :read_texts, through: :reads, source: :text
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
