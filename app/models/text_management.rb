237194a67169d9db00fc947e00467cdb0b87738d
YokoyamaGen <g.yokoyama.0616@gmail.com>
2021-04-03 14:35:15 +0900

    #38 usersテーブルと中間テーブルの関連付け、textsテーブルと中間テーブルの関連付け、中間テーブルに一意になるバリデーションを追加

--- app/models/text_management.rb
+++ app/models/text_management.rb
@@ -1,4 +1,8 @@
 class TextManagement < ApplicationRecord
   belongs_to :user
   belongs_to :text
+  validates :user_id, uniqueness: {
+    scope: :text_id,
+    message: "は同じテキストに2回以上読破済みにはできません"
+  }
 end
