a09663231f96b992b21867cc2a55d2de7d06c716
yukimi.y <yyy.yukimi@gmail.com>
2021-04-06 08:40:44 +0900

    text.rbとuser.rbを修正

--- app/models/text.rb
+++ app/models/text.rb
@@ -1,7 +1,5 @@
 class Text < ApplicationRecord
-  belongs_to :user
   has_many :reads, dependent: :destroy
-  has_many :read_users, through: :reads, source: :user
 
   validates :genre, presence: true
   validates :title, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
-  has_many :texts
   has_many :reads, dependent: :destroy
-  has_many :read_texts, through: :reads, source: :text
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
