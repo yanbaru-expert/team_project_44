96f019d39dcab771f95248c744c87ae09cea3101
miya <rinn.sasami@gmail.com>
2021-04-19 09:11:13 +0900

    application.jsでjQuery呼び出せるよう設定と動作確認

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,8 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap/dist/js/bootstrap"
 
+require('jquery')
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
